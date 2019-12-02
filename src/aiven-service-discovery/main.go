f8c2cf04895f095951f25798a031be4fb68c87e2
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-02 14:05:25 +0000

    Use new discoverer/fetcher/integrator in main.go
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/aiven-service-discovery/main.go
+++ src/aiven-service-discovery/main.go
@@ -4,11 +4,16 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/signal"
 	"time"
 
 	"code.cloudfoundry.org/lager"
+	"github.com/prometheus/client_golang/prometheus"
 
-	"github.com/alphagov/paas-observability-release/src/aiven-service-discovery/discovery"
+	d "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/discoverer"
+	f "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/fetcher"
+	i "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/integrator"
+	r "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/resolver"
 )
 
 const (
@@ -16,15 +21,19 @@ const (
 )
 
 var (
-	aivenAPIToken string
-	aivenProject  string
-	targetPath    string
+	aivenAPIToken              string
+	aivenProject               string
+	aivenPrometheusEndpointID  string
+	serviceDiscoveryTargetPath string
+
+	registry *prometheus.Registry
 )
 
 func main() {
 	flag.StringVar(&aivenAPIToken, "aiven-api-token", "", "Aiven API token use")
 	flag.StringVar(&aivenProject, "aiven-project", "", "Aiven project to discover")
-	flag.StringVar(&targetPath, "target-path", "", "File path to where targets will be written")
+	flag.StringVar(&aivenPrometheusEndpointID, "aiven-prometheus-endpoint-id", "", "Aiven Prometheus service integration endpoint to use")
+	flag.StringVar(&serviceDiscoveryTargetPath, "service-discovery-target-path", "", "File path to where targets will be written")
 	flag.Parse()
 
 	if aivenAPIToken == "" {
@@ -35,23 +44,52 @@ func main() {
 		log.Fatalf("Flag not specified: --aiven-project")
 	}
 
-	if targetPath == "" {
-		log.Fatalf("Flag not specified: --target-path")
+	if aivenPrometheusEndpointID == "" {
+		log.Fatalf("Flag not specified: --aiven-prometheus-endpoint-id")
+	}
+
+	if serviceDiscoveryTargetPath == "" {
+		log.Fatalf("Flag not specified: --service-discovery-target-path")
 	}
 
 	logger := lager.NewLogger("aiven-service-discovery")
 	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.INFO))
 
-	disco := discovery.Discoverer{
-		AivenAPIToken: aivenAPIToken,
-		AivenProject:  aivenProject,
-
-		Interval:   discoveryLoopInterval,
-		TargetPath: targetPath,
-
-		Logger: logger,
+	fetcher, err := f.NewFetcher(
+		aivenProject, aivenAPIToken,
+		logger, registry,
+	)
+	if err != nil {
+		log.Fatalf("Could not create fetcher: %s", err)
 	}
 
-	log.Println("Starting loop...")
-	disco.Loop()
+	integrator, err := i.NewIntegrator(
+		aivenProject, aivenAPIToken, aivenPrometheusEndpointID,
+		fetcher,
+		logger, registry,
+	)
+	if err != nil {
+		log.Fatalf("Could not create integrator: %s", err)
+	}
+
+	discoverer, err := d.NewDiscoverer(
+		aivenProject, serviceDiscoveryTargetPath,
+		fetcher, r.NewResolver(),
+		logger, registry,
+	)
+	if err != nil {
+		log.Fatalf("Could not create discoverer: %s", err)
+	}
+
+	fetcher.Start()
+	integrator.Start()
+	discoverer.Start()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	<-interrupt
+
+	fetcher.Stop()
+	integrator.Stop()
+	discoverer.Stop()
 }
