6fcd54586aea9b628d50b29763866fdb46780a7c
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-24 11:28:46 +0000

    Add listener for prometheus metrics
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/aiven-service-discovery/main.go
+++ src/aiven-service-discovery/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
 	"code.cloudfoundry.org/lager"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	d "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/discoverer"
 	f "github.com/alphagov/paas-observability-release/src/aiven-service-discovery/pkg/fetcher"
@@ -24,6 +28,7 @@ var (
 	aivenProject               string
 	aivenPrometheusEndpointID  string
 	serviceDiscoveryTargetPath string
+	prometheusListenPort       uint
 )
 
 func main() {
@@ -31,6 +36,7 @@ func main() {
 	flag.StringVar(&aivenProject, "aiven-project", "", "Aiven project to discover")
 	flag.StringVar(&aivenPrometheusEndpointID, "aiven-prometheus-endpoint-id", "", "Aiven Prometheus service integration endpoint to use")
 	flag.StringVar(&serviceDiscoveryTargetPath, "service-discovery-target-path", "", "File path to where targets will be written")
+	flag.UintVar(&prometheusListenPort, "prometheus-listen-port", 9274, "Port on which prometheus metrics will be exposed via /metrics")
 	flag.Parse()
 
 	if aivenAPIToken == "" {
@@ -47,6 +53,10 @@ func main() {
 
 	if serviceDiscoveryTargetPath == "" {
 		log.Fatalf("Flag not specified: --service-discovery-target-path")
+	}
+
+	if 0 == prometheusListenPort || prometheusListenPort > 65535 {
+		log.Fatalf("Flag invalid: --prometheus-listen-port must be between 1 and 65535")
 	}
 
 	logger := lager.NewLogger("aiven-service-discovery")
@@ -78,6 +88,18 @@ func main() {
 		log.Fatalf("Could not create discoverer: %s", err)
 	}
 
+	metricsServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", prometheusListenPort),
+		Handler: promhttp.Handler(),
+	}
+
+	go func() {
+		err := metricsServer.ListenAndServe()
+		if err != nil {
+			log.Fatalf("Could not listen and serve metrics: %s", err)
+		}
+	}()
+
 	fetcher.Start()
 	integrator.Start()
 	discoverer.Start()
@@ -89,4 +111,5 @@ func main() {
 	fetcher.Stop()
 	integrator.Stop()
 	discoverer.Stop()
+	metricsServer.Shutdown(context.Background())
 }
